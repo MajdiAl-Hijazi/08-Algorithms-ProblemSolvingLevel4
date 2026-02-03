b161db5384f2a2032392db08ab4358f6abb8ac33
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-02-03 18:35:15 +0300

    Add files via upload

---
+++ #09_YearCalendar.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+using namespace std;
+
+bool isLeapYear(short Year) {
+
+	return (Year % 4 == 0 && Year % 100 != 0) || (Year % 400 == 0);
+}
+
+short DayOfWeekOrder(short Day, short Month, short Year) {
+
+	short a, y, m;
+	a = (14 - Month) / 12;
+	y = Year - a;
+	m = Month + (12 * a) - 2;
+
+	return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+}
+
+short NumberOfDaysInAMonth(short Month, short Year) {
+
+	if (Month < 1 || Month>12)
+		return 0;
+
+	int days[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+
+	return (Month == 2) ? (isLeapYear(Year) ? 29 : 28) : days[Month - 1];
+}
+
+string MonthShortName(short MonthNumber) {
+
+	string Months[12] = { "Jan", "Feb", "Mar",
+	"Apr", "May", "Jun",
+	"Jul", "Aug", "Sep",
+	"Oct", "Nov", "Dec"
+	};
+
+	return (Months[MonthNumber - 1]);
+}
+
+void PrintMonthCalendar(short Month, short Year) {
+
+	int NumberOfDays;
+
+	// Index of the day from 0 to 6
+	int current = DayOfWeekOrder(1, Month, Year);
+	NumberOfDays = NumberOfDaysInAMonth(Month, Year);
+
+	printf("\n ____________%s____________\n\n", MonthShortName(Month).c_str());// ((( "c_str()" method use to convert string for C++ )))
+
+	printf(" Sun Mon Tue Wed Thu Fri Sat\n");
+
+	int i;
+	for (i = 0; i < current; i++)
+		printf("    ");
+
+	for (int j = 1; j <= NumberOfDays; j++) {
+
+		printf("%4d", j);
+
+		if (++i == 7) {
+			i = 0;
+			printf("\n");
+		}
+	}
+
+	printf("\n ___________________________\n");
+}
+
+short ReadYear() {
+
+	short Year;
+	cout << "\nPlease enter a year? ";
+	cin >> Year;
+	return Year;
+}
+
+void PrintYearCalendar(short Year) {
+
+	printf("\n ___________________________\n");
+	printf("\n       Calendar - %d        ", Year);
+	printf("\n ___________________________\n");
+
+	for (short i = 1; i <= 12; i++)
+		PrintMonthCalendar(i , Year);
+
+}
+
+int main() {
+
+	PrintYearCalendar(ReadYear());
+
+	system("pause>0");
+
+	return 0;
+}
