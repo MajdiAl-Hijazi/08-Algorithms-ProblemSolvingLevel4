b2999cf5940a144e212c8c275d7704ccab232733
Majdi AlHijazi <majdialhijazi@gmail.com>
2026-02-04 20:49:13 +0300

    Add files via upload

---
+++ #13_Date1LessThanDate2.cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+using namespace std;
+
+struct sDate {
+
+	short Year;
+	short Month;
+	short Day;
+};
+
+short ReadYear() {
+
+	short Year;
+	cout << "\nPlease enter a year ? ";
+	cin >> Year;
+	return Year;
+}
+
+short ReadMonth() {
+
+	short Month;
+	cout << "\nPlease enter a month ? ";
+	cin >> Month;
+	return Month;
+}
+
+short ReadDay() {
+
+	short Day;
+	cout << "\nPlease enter a day ? ";
+	cin >> Day;
+	return Day;
+}
+
+sDate ReadFullDate() {
+
+	sDate Date;
+	Date.Day = ReadDay();
+	Date.Month = ReadMonth();
+	Date.Year = ReadYear();
+	return Date;
+}
+
+bool IsDate1BeforeDate2(const sDate& Date1, const sDate& Date2) {
+
+	return ((Date1.Year < Date2.Year) ? true : (Date1.Year == Date2.Year) ? ((Date1.Month < Date2.Month) ? true : (Date1.Month == Date2.Month) ? Date1.Day < Date2.Day : false) : false);
+}
+
+int main() {
+
+	sDate Date1 = ReadFullDate();
+	cout << "\n";
+	sDate Date2 = ReadFullDate();
+
+	if (IsDate1BeforeDate2(Date1, Date2))
+		cout << "\nYes, Date1 is less than Date2.\n";
+	else
+		cout << "\nNo, Date1 isn't less than Date2.\n";
+
+
+	return 0;
+}
