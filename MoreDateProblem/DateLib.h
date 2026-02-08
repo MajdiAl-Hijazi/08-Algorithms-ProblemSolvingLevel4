8920bdd153a5af1a3d753f565e12cd2646d02c2b
Majdi <majdialhijazi@gmail.com>
2026-02-08 10:04:40 +0300

    add "DayOfWeekOrder" fun in .h file

--- MoreDateProblem/DateLib.h
+++ MoreDateProblem/DateLib.h
@@ -57,6 +57,16 @@ namespace ReadDate {
 
 namespace DateFun {
 
+    short DayOfWeekOrder(short Day, short Month, short Year) {
+
+        short a, y, m;
+        a = (14 - Month) / 12;
+        y = Year - a;
+        m = Month + (12 * a) - 2;
+
+        return (Day + y + (y / 4) - (y / 100) + (y / 400) + ((31 * m) / 12)) % 7;
+    }
+
     void SwapDate(stDate& Date1, stDate& Date2) {
 
         stDate Temp;
