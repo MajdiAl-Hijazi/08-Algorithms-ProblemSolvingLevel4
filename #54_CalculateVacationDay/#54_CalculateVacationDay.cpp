55dc8cdc0425d474bf59866b61cd622c3751a39d
Majdi <majdialhijazi@gmail.com>
2026-02-09 16:43:08 +0300

    update .gitignore file to ignore ths same extention, and add .cpp & .h file for "#54_CalculateVacationDay" folder

---
+++ #54_CalculateVacationDay/#54_CalculateVacationDay.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	cout << "Vacation Starts : ";
+	stDate DateFrom = ReadDate::ReadFullDate();
+
+	cout << "\n\nVacation Ends : ";
+	stDate DateTo = ReadDate::ReadFullDate();
+
+	cout << "\n\nVacation From : " << DateFun::DayShortName(DateFun::DayOfWeekOrder(DateFrom))
+		<< ", " << DateFrom.Day << "/" << DateFrom.Month << "/" << DateFrom.Year << endl;
+
+	cout << "\n\nVacation To : " << DateFun::DayShortName(DateFun::DayOfWeekOrder(DateTo))
+		<< ", " << DateTo.Day << "/" << DateTo.Month << "/" << DateTo.Year << endl;
+
+	cout << "\n\nActual Vacation Days is : " << DateFun::CalculateActualVacation(DateFrom, DateTo) << endl;
+
+	return 0;
+}
