cab253db45410117ff24801f44bdcb20e85abde6
Majdi <majdialhijazi@gmail.com>
2026-02-10 10:48:52 +0300

    finish this program

---
+++ #55_CalculateVacationReturnDate/#55_CalculateVacationReturnDate.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+short ReadNum(string Message) {
+
+	short Num;
+	cout << Message;
+	cin >> Num;
+	return Num;
+}
+
+int main() {
+
+	stDate Date = ReadDate::ReadFullDate();
+	cout << "\n";
+	short VacationDays = ReadNum("Please enter vacation days ? ");
+	Date = DateFun::CalculateVacationDate(Date, 23);
+	cout << "\nReturn Date : " << Date.Day << "/" << Date.Month << "/" << Date.Year << endl;
+
+
+	return 0;
+}
