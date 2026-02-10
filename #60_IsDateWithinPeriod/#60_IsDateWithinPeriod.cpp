7d0431cd96cfc72f1fa1c63c3869b5386f46c47e
Majdi <majdialhijazi@gmail.com>
2026-02-10 16:01:26 +0300

    finished IsDateWithinPeriod progrm

---
+++ #60_IsDateWithinPeriod/#60_IsDateWithinPeriod.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	cout << "Enter Period :\n";
+	stPeriod Period = PeriodFun::ReadPeriod();
+
+	cout << "\nEnter Date to Check :";
+	stDate Date = ReadDate::ReadFullDate();
+
+	if (PeriodFun::IsDateWithinPeriod(Period, Date))
+		cout << "\nYes, Date is Within Period\n";
+	else
+		cout << "\nNo, Date is not Within Period\n";
+
+	return 0;
+}
