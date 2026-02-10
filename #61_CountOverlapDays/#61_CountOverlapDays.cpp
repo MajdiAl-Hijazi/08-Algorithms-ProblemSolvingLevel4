bcd13e103855a853231ba834691ce5968035ee9d
Majdi <majdialhijazi@gmail.com>
2026-02-10 18:49:11 +0300

    finsh CountOverlapDays program

---
+++ #61_CountOverlapDays/#61_CountOverlapDays.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	cout << "Enter period 1 :\n";
+	stPeriod Period1 = PeriodFun::ReadPeriod();
+
+	cout << "\nEnter period 2 :\n";
+	stPeriod Period2 = PeriodFun::ReadPeriod();
+
+	cout << "\nOverlap Days count is : " << PeriodFun::CountOverlapDays(Period1, Period2) << endl;
+
+	return 0;
+}
