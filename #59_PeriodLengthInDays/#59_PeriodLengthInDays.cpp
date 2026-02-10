09eb9b512169a585391b4b9fe8a499db1bdb955c
Majdi <majdialhijazi@gmail.com>
2026-02-10 15:18:28 +0300

    add folder #59_PeriodLengthInDays and include program files .cpp & .h inside it

---
+++ #59_PeriodLengthInDays/#59_PeriodLengthInDays.cpp
@@ -0,0 +1,13 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	stPeriod Period = PeriodFun::ReadPeriod();
+
+	cout << "\nPeriod Length = " << PeriodFun::CalculatePeriodLengthInDays(Period) << endl;
+	cout << "Period Length (Include End Date) = " << PeriodFun::CalculatePeriodLengthInDays(Period, true) << endl;
+
+	return 0;
+}
