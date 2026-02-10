09a7e964329ac1cbe119d0b6da5862190447157d
Majdi <majdialhijazi@gmail.com>
2026-02-10 20:41:56 +0300

    complete IsValidateDate program

---
+++ #62_ValidateDate/#62_ValidateDate.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	stDate Date = ReadDate::ReadFullDate();
+
+	if (DateFun::IsValidateDate(Date))
+		cout << "\nYes, Date is a validate date.";
+	else
+		cout << "\nNo, Date is not a validate date.";
+
+	return 0;
+}
