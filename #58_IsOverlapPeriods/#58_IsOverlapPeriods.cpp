1e7957bf62621179d1aec2ad049dabaa4f8619bd
Majdi <majdialhijazi@gmail.com>
2026-02-10 12:52:25 +0300

    add new folder for IsOverlapPeriod problem and add .cpp & .h files, and into .h file add new namespace for period and into this namespace add `ReadPeriod` function

---
+++ #58_IsOverlapPeriods/#58_IsOverlapPeriods.cpp
@@ -0,0 +1,14 @@
+﻿#include<iostream>
+#include "DateLib.h"
+using namespace std;
+
+int main() {
+
+	cout << "Enter Period 1:" << endl;
+	stPeriod Period1 = PeriodFun::ReadPeriod();
+
+	cout << "\nEnter Period 2:" << endl;
+	stPeriod Period2 = PeriodFun::ReadPeriod();
+
+	return 0;
+}
